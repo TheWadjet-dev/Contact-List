3076df81af104bc1503e4fe2b4dcd1e963a924b2
Stiwar Saltos <69290901+TheWadjet-dev@users.noreply.github.com>
2024-11-13 00:54:11 -0500

    udpate app

--- Gemfile
+++
@@ -1,5 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'sinatra'
-gem 'thin' # Opcional: el servidor web Thin es ligero y rápido para producción
-gem 'rack', '~> 2.0' # Necesario para trabajar con Sinatra y gestionar peticiones HTTP
--- app.rb
+++ app.rb
@@ -1,86 +1,24 @@
 require 'sinatra'
-require 'sinatra/reloader' if development?
+require 'tzinfo'
 
-# In-memory storage for contacts
-class Contact
-  attr_accessor :name, :email, :phone
+# Definir las zonas horarias
+ZONES = [
+  'America/New_York',  # Nueva York (EE.UU.)
+  'Europe/London',     # Londres (Reino Unido)
+  'Asia/Tokyo',        # Tokio (Japón)
+  'Australia/Sydney',  # Sídney (Australia)
+  'Africa/Johannesburg', # Johannesburgo (Sudáfrica)
+  'Europe/Paris'       # París (Francia)
+]
 
-  def initialize(name, email, phone)
-    @name = name
-    @email = email
-    @phone = phone
-  end
-end
-
-class Agenda
-  def initialize
-    @contacts = []
-  end
-
-  def all
-    @contacts
-  end
-
-  def add(contact)
-    @contacts << contact
-  end
-
-  def find(index)
-    @contacts[index]
-  end
-
-  def update(index, name, email, phone)
-    contact = @contacts[index]
-    contact.name = name
-    contact.email = email
-    contact.phone = phone
-  end
-
-  def delete(index)
-    @contacts.delete_at(index)
-  end
-end
-
-agenda = Agenda.new
-
-# Root route - show the list of contacts
+# Página principal que carga el reloj mundial
 get '/' do
-  @contacts = agenda.all
   erb :index
 end
 
-# Route to show the form to add a new contact
-get '/contacts/new' do
-  erb :new
-end
-
-# Route to add a new contact
-post '/contacts' do
-  name = params[:name]
-  email = params[:email]
-  phone = params[:phone]
-  contact = Contact.new(name, email, phone)
-  agenda.add(contact)
-  redirect '/'
-end
-
-# Route to show the form to edit an existing contact
-get '/contacts/:index/edit' do
-  @contact = agenda.find(params[:index].to_i)
-  erb :edit
-end
-
-# Route to update an existing contact
-put '/contacts/:index' do
-  name = params[:name]
-  email = params[:email]
-  phone = params[:phone]
-  agenda.update(params[:index].to_i, name, email, phone)
-  redirect '/'
-end
-
-# Route to delete a contact
-delete '/contacts/:index' do
-  agenda.delete(params[:index].to_i)
-  redirect '/'
+# API para obtener la hora actual en una zona horaria específica
+get '/time/:zone' do
+  timezone = TZInfo::Timezone.get(params[:zone])
+  current_time = timezone.now.strftime("%Y-%m-%d %H:%M:%S")
+  current_time.to_json
 end
