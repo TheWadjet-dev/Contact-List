93ebba9f1d0c7409f260b3a10a61015915c74086
Stiwar Saltos <69290901+TheWadjet-dev@users.noreply.github.com>
2024-11-13 15:08:59 -0500

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,24 +1,31 @@
-require 'sinatra'
-require 'tzinfo'
+# Import necessary libraries
+require 'timezone'
+require 'tty-prompt'
+require 'time'
 
-# Definir las zonas horarias
-ZONES = [
-  'America/New_York',  # Nueva York (EE.UU.)
-  'Europe/London',     # Londres (Reino Unido)
-  'Asia/Tokyo',        # Tokio (Japón)
-  'Australia/Sydney',  # Sídney (Australia)
-  'Africa/Johannesburg', # Johannesburgo (Sudáfrica)
-  'Europe/Paris'       # París (Francia)
-]
+class WorldClock
+  def initialize
+    @prompt = TTY::Prompt.new
+    @zones = ['America/New_York', 'Europe/London', 'Asia/Tokyo', 'Australia/Sydney', 'America/Los_Angeles', 'Europe/Madrid']
+  end
 
-# Página principal que carga el reloj mundial
-get '/' do
-  erb :index
+  def show_menu
+    puts "\nWelcome to the World Clock!"
+    while true
+      city = @prompt.select("Select a city to see the time:", @zones)
+      show_time(city)
+      continue = @prompt.yes?("Would you like to check another city?")
+      break unless continue
+    end
+  end
+
+  def show_time(city)
+    zone = Timezone::Zone.new :zone => city
+    time = zone.time Time.now
+    formatted_time = time.strftime("%H:%M:%S")
+    puts "\nThe time in #{city.split('/').last.gsub('_', ' ')} is: #{formatted_time}"
+  end
 end
 
-# API para obtener la hora actual en una zona horaria específica
-get '/time/:zone' do
-  timezone = TZInfo::Timezone.get(params[:zone])
-  current_time = timezone.now.strftime("%Y-%m-%d %H:%M:%S")
-  current_time.to_json
-end
+# Run the app
+WorldClock.new.show_menu
